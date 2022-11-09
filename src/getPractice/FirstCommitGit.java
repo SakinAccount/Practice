81b06f66ee86bd59bdcc4c225f42406f8a99e7ff
parviz <pamir0986@gmail.com>
2022-11-09 17:19:09 -0500

    second commit for remote

--- src/getPractice/FirstCommitGit.java
+++ src/getPractice/FirstCommitGit.java
@@ -1,5 +1,11 @@
 package getPractice;
 
-public class FirstCommitGit {
-
+   public class FirstCommitGit {
+   public static void main(String[] args) {
+	
 }
+
+public void test() {
+	System.out.println("This is from local repo");
+}
+   }
